05bd9871f76fcef060d9d201602e4ebeecaed0d9
Vincent Friedl <vincent.friedl@dawsoncollege.qc.ca>
2024-09-25 17:18:39 -0400

    First try code :D

---
+++ grade_Caclulator.py
@@ -0,0 +1,15 @@
+LabPP = int(input("Enter the number of labs completed"))
+    if LabPP(LabPP <=6):
+        LabPP = 20
+    else: 
+
+Quizzes = int(input("Enter the number of quizzes completed"))
+Ass1 = float(input("Enter grade for Assignment 1"))
+Ass2 = float(input("Enter grade for Assignment 2"))
+Ass3 = float(input("Enter grade for Assignment 3"))
+Ass4 = float(input("Enter grade for Assignment 4"))
+AssAvg = ((Ass1 + Ass2 + Ass3 + Ass4)/4)
+MidT1 = input("Enter grade for Midterm 1")
+MidT2 = input("Enter grade for Midterm 2")
+MidTAvg = ((MidT1 + MidT2)/2)
+FinalE = input(
